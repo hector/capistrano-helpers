d8440b902834f1de7bab3bca59fe2560a0e2b783
Hector Parra <wizbcn@gmail.com>
2012-08-28 14:19:02 +0200

    Create shared directories after setup

--- lib/capistrano-helpers/shared.rb
+++ lib/capistrano-helpers/shared.rb
@@ -15,8 +15,20 @@ CapistranoHelpers.with_configuration do
         run "rm -rf #{release_path}/#{path} && ln -nfs #{shared_path}/#{path} #{release_path}/#{path}"
       end
     end
+    
+    desc '[internal] Create the defined shared directories'
+    task :create_shared_dirs do
+      if !exists?(:shared)
+        abort 'You must specify which files to symlink using the "set :shared" command.'
+      end
+      shared.each do |path|
+        shared_dir = File.join(shared_path, path)
+        run "#{try_sudo} mkdir -p #{shared_dir}"
+        run "#{try_sudo} chmod o-rwx #{shared_dir}"
+      end
+    end    
   end
 
   before "deploy:finalize_update", "deploy:symlink_shared"
-
+  after "deploy:setup", "deploy:create_shared_dirs"
 end
